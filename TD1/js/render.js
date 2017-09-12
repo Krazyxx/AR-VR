fde432b869699d075b8029b4ee2ba1e815b4a77b
Jimmy Gouraud <jimmy.gouraud@hotmail.fr>
2017-09-12 11:23:21 +0200

    add description 1. on the website + beginning of 2.

--- TD1/js/render.js
+++ TD1/js/render.js
@@ -1,7 +1,7 @@
 var container, stats;
 var scene, camera, renderer;
 var controls, clock;
-var mat1, mat2, mat3, mat4, mat5;
+var mat0, mat1, mat2, mat3, mat4, mat5;
 
 function init()
 {
@@ -22,13 +22,19 @@ function init()
     document.body.appendChild(renderer.domElement);
 
     // controls
-    controls = new THREE.TrackballControls(camera, renderer.domElement);
+    //controls = new THREE.TrackballControls(camera, renderer.domElement);
+    controls = new THREE.PointerLockControls(camera, renderer.domElement);
+    controls.enabled = true;
+    scene.add(controls.getObject());
+    
     clock = new THREE.Clock();
 
     
     // Load textures
     var textureLoader = new THREE.TextureLoader();
 
+    mat0 = new THREE.MeshBasicMaterial(
+	{color: 0x00ff00});
     mat1 = new THREE.MeshBasicMaterial(
 	{map: textureLoader.load("textures/tnt1.png")});
     mat2 = new THREE.MeshBasicMaterial(
@@ -40,39 +46,120 @@ function init()
     mat5 = new THREE.MeshBasicMaterial(
 	{map: textureLoader.load("textures/sand.png")});
     
-    // Create map
+    // Create scene
+    //load_scene_1();
+    //load_scene_2();
+    load_scene_3();
+
+}
+
+function animate()
+{
+    stats.update();
+    
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+    //controls.update(clock.getDelta());
+}
+    
+document.addEventListener("keydown", function(event){
+    if      (event.keyCode == KEY_Z) { move_up();    }
+    else if (event.keyCode == KEY_S) { move_down();  }
+    else if (event.keyCode == KEY_Q) { move_right(); }
+    else if (event.keyCode == KEY_D) { move_left();  }
+});
+
+
+KEY_Z		= 90;
+KEY_Q		= 81;
+KEY_S		= 83;
+KEY_D		= 68;
+
+function move_up()    { controls.getObject().position.z -= 0.1; }
+function move_down()  { controls.getObject().position.z += 0.1; }
+function move_right() { controls.getObject().position.x -= 0.1; }
+function move_left()  { controls.getObject().position.x += 0.1; }
+
+
+function load_scene_1()
+{
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var cube = new THREE.Mesh(geometry, mat0);
+
+    scene.add(cube);
+    
+    cube.rotation.x += 0.5;
+    cube.rotation.y += 0.5;
+    
+    camera.position.z = 5;
+}
+
+function load_scene_2()
+{
     var geometry = new THREE.BoxGeometry(1, 1, 1);
     
     var mat_tnt = [mat1, mat1, mat2, mat2, mat1, mat1];
     var tnt = new THREE.Mesh(geometry, mat_tnt);
-    tnt.position.set(0, 2, 0);
     scene.add(tnt);
 
+    var mat_stone = [mat3, mat4];
     for (var x = -10; x <= 10; x++) {
 	for (var z = -10; z <= 10; z++) {
 	    var sand = new THREE.Mesh(geometry, mat5);
 	    sand.position.set(x, -2, z);
 	    scene.add(sand);
 	    
-	    var stone;
-	    if (Math.floor(Math.random() * 2)) {
-		stone = new THREE.Mesh(geometry, mat3);
-	    } else {
-		stone = new THREE.Mesh(geometry, mat4);
-	    }
+	    var rand = Math.floor(Math.random() * mat_stone.length);
+	    var stone = new THREE.Mesh(geometry, mat_stone[rand]);
 	    stone.position.set(x, -1, z);
 	    scene.add(stone);
 	}
     }
-
-    create_wall(-10,0,-10,21,4, 1,[mat3, mat4]);
-    create_wall(-10,0,-10, 1,4,21,[mat3, mat4]);
-    create_wall(-10,0, 10,21,4, 1,[mat3, mat4]);
-    create_wall( 10,0,-10, 1,4,21,[mat3, mat4]);
     
     // Place camera
-    camera.position.z = 20;
+    camera.position.z = 30;
     camera.position.y = 10;
+}
+
+function load_scene_3()
+{
+    load_scene_2();
+    
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    mat_stone = [mat3, mat4];
+    
+    // walls
+    create_wall(-10,0,-10,21,4, 1,mat_stone);
+    create_wall(-10,0,-10, 1,4,21,mat_stone);
+    create_wall( 10,0,-10, 1,4,21,mat_stone);
+
+    // door
+    create_wall(  2,0, 10, 9,4, 1,mat_stone);
+    create_wall(-10,0, 10, 9,4, 1,mat_stone);
+    create_wall( -2,3, 10, 9,1, 1,mat_stone);
+
+    // tower
+    create_wall( -2,0,-2, 5,8,1,mat_stone);
+    create_wall(  2,0,-2, 1,8,5,mat_stone);
+    create_wall( -2,0, 2, 5,8,1,mat_stone);
+    create_wall( -2,0,-2, 1,8,5,mat_stone);
+
+    // drawbridge
+    create_wall( -2,-1,11, 1,1,5,mat_stone);
+    create_wall(  2,-1,11, 1,1,5,mat_stone);
+    create_wall( -2,-2,11, 1,0,5,mat_stone);
+    create_wall(  2,-2,11, 1,0,5,mat_stone);
+    
+    for (var x = -1; x <= 1; x++) {
+	for (var z = 11; z < 16; z++) {
+	    var rand = Math.floor(Math.random() * mat_stone.length);
+	    var stone = new THREE.Mesh(geometry, mat_stone[rand]);
+	    stone.position.set(x, -1, z);
+	    scene.add(stone);
+	}
+    }
+    
 }
 
 function create_wall(pos_x, pos_y, pos_z,lenght,height,width, mat)
@@ -103,14 +190,4 @@ function create_wall(pos_x, pos_y, pos_z,lenght,height,width, mat)
 	stone.position.set(pos_x+lenght-1, pos_y+height, pos_z+z);
 	scene.add(stone);
     }
-}
-
-function animate()
-{
-    stats.update();
-    
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    
-    controls.update(clock.getDelta());
 }
