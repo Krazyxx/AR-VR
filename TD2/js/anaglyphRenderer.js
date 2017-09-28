0198ae71ff5d1e04c29ce009fd0d961af361b15a
Jimmy Gouraud <jimmy.gouraud@hotmail.fr>
2017-09-28 17:30:48 +0200

    Begin TD2 - 1.1

--- TD2/js/anaglyphRenderer.js
+++ TD2/js/anaglyphRenderer.js
@@ -1,18 +1,60 @@
 function AnaglyphRenderer ( renderer ) {
 
-  // left and right cameras
-  this.cameraLeft  = new THREE.Camera();
-  this.cameraLeft.matrixAutoUpdate = false;
-  this.cameraRight = new THREE.Camera();
-  this.cameraRight.matrixAutoUpdate = false;
+    // left and right cameras
+    this.cameraLeft  = new THREE.Camera();
+    this.cameraLeft.matrixAutoUpdate = false;
+    this.cameraRight = new THREE.Camera();
+    this.cameraRight.matrixAutoUpdate = false;
 
 
-	this.update = function ( camera ) {
-    // TODO
-	}
+    this.update = function ( camera ) {
+        // TODO
+        camera.updateMatrixWorld();
+        this.cameraRight.matrixWorld = camera.matrixWorld.clone();
+        this.cameraLeft.matrixWorld = camera.matrixWorld.clone();
 
-  this.render = function ( scene, camera ) {
-    // TODO
-  }
+        var znear = camera.near;
+        var zfar = camera.far;
 
+        var d = displayParameters.distanceScreenViewer;
+        var h = displayParameters.screenSize().x; // height
+        var w = displayParameters.screenSize().y; // width
+        var ipd = displayParameters.ipd;
+
+        var top = znear * (h / (2.0 * d));
+        var bottom = - znear * (h / (2.0 * d));
+
+        // left eye
+        var L_right = znear * ((w + ipd) / (2.0 * d));
+        var L_left = - znear * ((w - ipd) / (2.0 * d));
+
+        // right eye
+        var R_right = znear * ((w - ipd) / (2.0 * d));
+        var R_left = -znear * ((w + ipd) / (2.0 * d));
+
+
+//        var translateLeft = new THREE.Matrix4();
+//        translateLeft.makeTranslation(-displayParameters.ipd/2, 0, 0);
+//        this.cameraLeft.matrixWorld.multiplyMatrices(this.cameraLeft.matrixWorld, translateLeft);
+        this.cameraLeft.projectionMatrix.makePerspective(L_left, L_right, top, bottom, znear, zfar);
+
+//        var translateRight = new THREE.Matrix4();
+//        translateRight.makeTranslation(displayParameters.ipd/2, 0, 0);
+//        this.cameraRight.matrixWorld.multiplyMatrices(this.cameraRight.matrixWorld, translateLeft);
+        this.cameraRight.projectionMatrix.makePerspective(R_left, R_right, top, bottom, znear, zfar);
+    }
+
+    this.cpt = 0;
+
+    this.render = function ( scene, camera ) {
+        this.update(camera);
+
+        if (this.cpt == 0) {
+            this.cpt = 1;
+            renderer.render(scene,  this.cameraRight);
+        } else {
+            this.cpt = 0;
+            renderer.render(scene,  this.cameraLeft);
+        }
+    }
 }
--- TD2/js/displayParameters.js
+++ TD2/js/displayParameters.js
@@ -1,25 +1,30 @@
 var displayParameters = {
 
-  // parameters for stereo rendering
-  // physical screen diagonal -- in mm
-  screenDiagonal: 337.82,
-  screenResolutionWidth: 1440,
-  aspectRatio: 1.6,
 
-  // inter pupillar distance -- in mm
-  ipd: 64,
+    // parameters for stereo rendering
+    // physical screen diagonal -- in mm
+    screenDiagonal: 685.8,
+    screenResolutionWidth: 2560,
+    aspectRatio: 16 / 9,
 
-  // distance bewteen the viewer and the screen -- in mm
-  distanceScreenViewer: 500,
+    // inter pupillar distance -- in mm
+    ipd: 64,
 
-  // TODO: amount of distance in mm between adjacent pixels
-  pixelPitch: function() {
+    // distance bewteen the viewer and the screen -- in mm
+    distanceScreenViewer: 500,
 
-  },
+    // TODO: amount of distance in mm between adjacent pixels
+    pixelPitch: function() {
+        var screenResolutionHeight = this.screenResolutionWidth / this.aspectRatio;
+        var screenResolutionDiagonal = Math.sqrt(this.screenResolutionWidth * this.screenResolutionWidth + screenResolutionHeight * screenResolutionHeight);
+        return screenResolutionDiagonal / this.screenDiagonal;
+    },
 
-  //TODO: physical display width and height -- in mm
-  screenSize: function() {
-    return new THREE.Vector2(300,200);
-  }
-  
+    //TODO: physical display width and height -- in mm
+    screenSize: function() {
+        var pixelPitch = this.pixelPitch();
+        var screenResolutionHeight =  this.screenResolutionWidth / this.aspectRatio;
+        return new THREE.Vector2(this.screenResolutionWidth / pixelPitch, screenResolutionHeight / pixelPitch);
+    }
+
 };
